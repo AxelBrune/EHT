9b4030360f923a67a43e97631a799ef6efb5613a
Axel Brune <axelbrune50@gmail.com>
2020-01-15 19:38:45 +0100

    Correction labels de la page de création d'annonce

--- Site/creerAnnonce.php
+++ Site/creerAnnonce.php
@@ -12,20 +12,24 @@
         <form class="col s12" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
             <div class="row col s6 l6 m6 offset-l3">
                 <div class="input-field">
-                    <input placeholder="Entrer un titre" id="titre" type="text" name="titre">
                     <label for="nom">Titre : </label>
+                    <input placeholder="Entrer un titre" id="titre" type="text" name="titre">
                 </div>
                 <div class="input-field">
-                    <input id="secteur" type="text" placeholder="Entrer un secteur" name="secteur">
                     <label for="secteur">Secteur :</label>
+                    <input id="secteur" type="text" placeholder="Entrer un secteur" name="secteur">
                 </div>
                 <div class="input-field">
-                    <input id="prix" type="text" placeholder="Entrer un prix" name="prix">
                     <label for="prix">Prix :</label>
+                    <input id="prix" type="text" placeholder="Entrer un prix" name="prix">
                 </div>
                 <div class="input-field">
-                    <textarea name="description" id="description" cols="30" rows="10"></textarea>
-                    <label for="description">Description :</label>
+                    <label for="description">Description :</label><br>
+                    <textarea name="description" id="description" cols="13" rows="3"></textarea>
+                </div>
+                <div class="input-field">
+                    <label for="photo">Ajouter des photos :</label>
+                    <input type="file" name="photo" id="photo" />
                 </div>
 			</div>
             <div class="row col s6 l6 offset-l5">
